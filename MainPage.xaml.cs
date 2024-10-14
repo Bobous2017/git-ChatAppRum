d21ebbb8e949826d2c6ac944f05b7079eac89a05
B-2017 <107534702+Bobous2017@users.noreply.github.com>
2024-10-14 10:15:16 +0200

    Titel & Update

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,9 +14,9 @@
             count++;
 
             if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+                CounterBtn.Text = $"Clicked {count} Time";
             else
-                CounterBtn.Text = $"Clicked {count} times";
+                CounterBtn.Text = $"Clicked {count} Times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
